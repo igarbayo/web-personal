a0445c000e55ab1bc07eb15fe0be5125dacaae61
igarbayo <ignacio.garbayo@rai.usc.es>
2026-06-11 21:42:15 +0200

    images

--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const basePath = '/web-personal'
+
 const nextConfig = {
   output: 'export',
-  basePath: '/web-personal',
+  basePath,
   images: { unoptimized: true },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
+  },
 }
 
 module.exports = nextConfig
